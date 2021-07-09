d043c0df3f90c128a9f7653ff3fe859d9bf18ee8
thephotogenicbug <navinkumarachari@gmail.com>
2021-07-09 22:07:47 +0530

    Allow cors

--- server.js
+++ server.js
@@ -7,16 +7,27 @@ const port = process.env.PORT;
 const app = express();
 const server = http.createServer(app);
 const Routes = require("./app/routes");
-app.use(express.json());
 
-app.use(cors())
+
 app.use([
     bodyParser.json(),
     bodyParser.urlencoded({extended:false}),
     Routes
 ])
 
-const io = require("socket.io")(server)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+  });
+
+const io = require("socket.io")(server, {
+    cors: {
+      origin: '*',
+    }
+});
 const socketManager = require("./app/socketManager");
 io.on("connection", socketManager);
 
